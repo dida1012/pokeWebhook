8fc3a493c28e917943f5be5731b0d93eaa04b752
David Diener <daviddiener@outlook.com>
2019-04-01 23:27:08 +0200

    refactored

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module['exports'] = function helloWorld (hook) {
   var intent = hook.req.body.queryResult.intent['displayName'];
   var content = hook.req.body.queryResult.parameters['pokemon'];
   request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+content+'/', true)
+
   request.onload = function() {
     var data = JSON.parse(this.responseText)
     if (request.status >= 200 && request.status < 400) {
