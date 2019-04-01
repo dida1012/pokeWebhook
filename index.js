ec80827bffcbf261073c1cc439dc804dbd6eaf79
David Diener <daviddiener@outlook.com>
2019-04-01 23:40:19 +0200

    refactored

--- index.js
+++ index.js
@@ -21,3 +21,20 @@ module['exports'] = function whatthefuck (hook) {
   }
   request.send()
 }
+
+module['exports'] = function Height (hook) {
+  var intent = hook.req.body.queryResult.intent['displayName'];
+  var content = hook.req.body.queryResult.parameters['pokemon'];
+  request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+content+'/', true)
+
+  request.onload = function() {
+    var data = JSON.parse(this.responseText)
+    if (request.status >= 200 && request.status < 400) {  
+         hook.res.json({"fulfillmentText": "SECOND FUNCTION TRIGGERED"});
+         hook.res.end();
+        } else {
+      console.log('error')
+    }
+  }
+  request.send()
+}
