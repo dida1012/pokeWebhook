a592ac271d4de74c1283cdf64a6737baa9693545
Stefan Beuchert <best1029@HS-Karlsruhe.de>
2019-04-01 10:56:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto/', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.responseText)
+
+  if (request.status >= 200 && request.status < 400) {
+      console.log(data.weight)
+  } else {
+    console.log('error')
+  }
+}
+
+request.send()
