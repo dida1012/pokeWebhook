8d6b011a2ae4c1eebecb19a48abea37bf63baa65
David Diener <daviddiener@outlook.com>
2019-04-01 22:58:06 +0200

    quicktesting

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ module['exports'] = function helloWorld (hook) {
   var content = hook.req.body.queryResult.parameters['pokemon'];
   request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+content+'/', true)
   request.onload = function() {
-    // Begin accessing JSON data here
     var data = JSON.parse(this.responseText)
-    
     if (request.status >= 200 && request.status < 400) {
           hook.res.json({"fulfillmentText": data.abilities[0].ability.name});
           hook.res.end();
-          
-
         } else {
       console.log('error')
-      hook.res.json({"fulfillmentText": data.weight});
     }
   }
   request.send()
