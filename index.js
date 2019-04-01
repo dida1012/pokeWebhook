3b175e1653672919d73d10ae6d92bf18e7bed0c3
David Diener <daviddiener@outlook.com>
2019-04-01 20:46:07 +0200

    sd

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest()
 
-// request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto/', true)
-// request.onload = function() {
-//   // Begin accessing JSON data here
-//   var data = JSON.parse(this.responseText)
-
-//   if (request.status >= 200 && request.status < 400) {
-//       console.log(data.weight)
-//   } else {
-//     console.log('error')
-//     hook.res.json({"fulfillmentText": data.weight});
-//   }
-// }
-
 module['exports'] = function helloWorld (hook) {
-  // var content = hook.req.body.queryResult.parameters['content'];
-  hook.res.json({"fulfillmentText": "asd"});
-  hook.res.end();
-};
+  request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto/', true)
+  request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.responseText)
+
+    if (request.status >= 200 && request.status < 400) {
+          hook.res.json({"fulfillmentText": "asd"});
+          hook.res.end();
+    } else {
+      console.log('error')
+      hook.res.json({"fulfillmentText": data.weight});
+    }
+  }
+}
+
+// module['exports'] = function helloWorld (hook) {
+//   // var content = hook.req.body.queryResult.parameters['content'];
+//   hook.res.json({"fulfillmentText": "asd"});
+//   hook.res.end();
+// };
 
 // request.send()
